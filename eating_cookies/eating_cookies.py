5caed78926bf6c4f3e3d52000326eaa060f75262
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-02-20 11:05:51 -0800

    put cache inside eating_cookies instead of global; passing all tests in around 0.002s

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,31 +5,30 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-cache = {}
-def eating_cookies(n, cache=cache):
-  ways = 0
-  print(f'n: {n}')
-  if n == 1 or n == 0:
-    print("\nn == 1 or n == 0\n")
+# cache = {}
+def eating_cookies(n, cache=None):
+  # ways = 0
+  # print(f'n: {n}')
+  if n == 0:
+    # print("\nn == 1 or n == 0\n")
     return 1
   elif n < 0:
-    print("\nn < 0\n")
+    # print("\nn < 0\n")
     return 0
-  elif n in cache:
+  elif cache and cache[n] > 0:
     return cache[n]
   else:
-    for i in range(1, 4)[::-1]:
-      print(f"i: {i}")
-      print(f'n-i: {n-i}')
-      print(f"\nWays before eating_cookies recursion: {ways}\n")
-      ways += eating_cookies(n-i)
-      print(f"\nWays after eating_cookies recursion: {ways}\n")
-  cache[n] = ways
-  return ways
+    if cache is None:
+      cache = {}
+      # print(cache)
+    ways = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    cache[n] = ways
+    # print(cache)
+    return ways
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+    print("There are only {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
     print('Usage: eating_cookies.py [num_cookies]')
