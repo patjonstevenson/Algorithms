2b476d43345a5cbe468f7d192b58de66f2eb3c40
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-02-20 09:39:39 -0800

    have eating cookies passing by using caching

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,27 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+cache = {}
+def eating_cookies(n, cache=cache):
+  ways = 0
+  print(f'n: {n}')
+  if n == 1 or n == 0:
+    print("\nn == 1 or n == 0\n")
+    return 1
+  elif n < 0:
+    print("\nn < 0\n")
+    return 0
+  elif n in cache:
+    return cache[n]
+  else:
+    for i in range(1, 4)[::-1]:
+      print(f"i: {i}")
+      print(f'n-i: {n-i}')
+      print(f"\nWays before eating_cookies recursion: {ways}\n")
+      ways += eating_cookies(n-i)
+      print(f"\nWays after eating_cookies recursion: {ways}\n")
+  cache[n] = ways
+  return ways
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
