518de059b92d683c7d50414e6f899b60a78b645f
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-02-21 17:23:42 -0800

    wrote sum_items_capacity helper function; still trying to set up base cases for knapsack_solver

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -2,11 +2,35 @@
 
 import sys
 from collections import namedtuple
+from functools import reduce
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
+def sum_items_capacity(items):
+  return reduce(lambda x,y: x + y[1], items, 0)
+
 def knapsack_solver(items, capacity):
-  pass
+  # # if not items:
+  # #   return 
+  # sorted_items = sorted(items, key=lambda x: x[2], reverse=True)
+  # print(f"Sorted items: {[item[2]/item[1] for item in sorted_items]}")
+  # selected = []
+  # remaining_capacity = capacity
+  # value = 0
+  # i = 0
+  # while remaining_capacity and i < len(sorted_items):
+  #   if remaining_capacity - sorted_items[i][1] >= 0:
+  #     selected.append(sorted_items[i])
+  #     remaining_capacity -= sorted_items[i][1]
+  #     value += sorted_items[i][2]
+  #   i += 1
+  # return {'Value': value, 'Chosen': [item[0] for item in selected]}
+  if not items:
+    return items
+  elif sum_items_capacity > capacity:
+    return 
+  else:
+    return
   
 
 if __name__ == '__main__':
